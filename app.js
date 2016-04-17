f0aebba8df5d8343fde9be6dcc8daef4d87cdaff
Mitch Grove <mgrove96@gmail.com>
2016-04-17 15:37:48 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+$(function () {
+
+
+	for (var i = 1; i <= 100; i++)
+	{
+		if (i % 3 === 0 | i % 5 === 0)
+		{
+			if (i % 3 === 0 & i % 5 === 0)
+			{
+				$('body').append("<p>FizzBuzz</p>");
+			}
+			else if (i % 3 === 0)
+			{
+				$('body').append("<p>Fizz</p>");
+			}
+			else
+			{
+				$('body').append("<p>Buzz</p>");
+			}
+		}
+		else
+		{
+			$('body').append("<p>"+i+"</p>");
+		}
+	}
+});
